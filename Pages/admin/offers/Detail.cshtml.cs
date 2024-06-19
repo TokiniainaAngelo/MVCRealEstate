9dfba38fc6c51045744476ed02cf8163143eacd3
Ramananjoelina Tokiniaina Angelo <ramananjoelinatokiniaina@gmail.com>
2024-06-19 21:20:55 +0300

    make navigation work

---
+++ Pages/admin/offers/Detail.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MVCRealEstate.Data;
+using MVCRealEstate.Models;
+
+namespace MVCRealEstate.Pages.admin.offers
+{
+    public class DetailModel : PageModel
+    {
+		private readonly MVCRealEstateContext _context;
+
+		public DetailModel(MVCRealEstateContext context)
+		{
+			_context = context;
+		}
+
+		public Offer Offer { get; set; }
+
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			Offer = await _context.Offer
+				.Include(o => o.Location)
+				.Include(o => o.OfferMedias)
+				.Include(o => o.Agency)
+				.Include(o => o.OwnerInfo)
+				.FirstOrDefaultAsync(m => m.OfferId == id);
+
+			if (Offer == null)
+			{
+				return NotFound();
+			}
+			return Page();
+		}
+	}
+}
